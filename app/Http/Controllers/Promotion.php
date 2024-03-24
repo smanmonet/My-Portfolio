e395246e7d47941303e1db03307924c37488d6da
Faalukk <zelacer.tv@gmail.com>
2024-03-24 17:57:39 +0700

    uploadController

---
+++ app/Http/Controllers/Promotion.php
@@ -0,0 +1,31 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductRealModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use Carbon\Carbon;
+
+class Promotion extends Controller
+{
+    public function index()
+    {
+        $emp = DB::table('promotion')->get();
+        //dd($emp);
+        return view('show_promotion',compact('emp'));
+    }
+    
+    function delete($id){
+        DB::table('promotion')->where('proID',$id)->delete();
+        return redirect('/Promotion');
+    }
+    function info($id){
+        $PromoInfo=DB::table('promotion')->where('proID',$id)->first();
+        $PdInfo=DB::table('infodetail')->where('promotionID',$id)->get();
+        //dd($PdInfo);
+        return view('infoPromotion',compact('PromoInfo','PdInfo'));
+    }
+}
